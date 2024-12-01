ffac80e9011b2851add72000f980b2d4550a9f93
Roland Liechti <rliechti@silvermail.ch>
2024-12-01 20:01:10 +0100

    change title tag

--- client/router.js
+++ client/router.js
@@ -40,7 +40,7 @@ const router = createRouter({
 });
 
 router.afterEach((to) => {
-  let title = "flatnotes";
+  let title = "wunschliste";
   if (to.name === "note") {
     if (to.params.title) {
       title = `${to.params.title} - ${title}`;
