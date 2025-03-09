3b9d61b566fe5786c6ff8e90d00ebf95e29b67f0
Nathanael Liechti <technat@technat.ch>
2025-03-09 18:23:47 +0100

    fix: shortcuts should use ctrl+alt h or n

--- client/App.vue
+++ client/App.vue
@@ -48,15 +48,15 @@ Mousetrap.bind("/", () => {
   }
 });
 
-// 'CTRL/CMD + SHIFT + N' to create new note
-Mousetrap.bindGlobal("mod+shift+n", () => {
+// 'CTRL + ALT/OPT + N' to create new note
+Mousetrap.bindGlobal("ctrl+alt+n", () => {
   if (route.name !== "login") {
     router.push({ name: "new" });
     return false;
   }
 });
-// 'CTRL/CMD + SHIFT + H' to go to home
-Mousetrap.bindGlobal("mod+shift+h", () => {
+// 'CTRL + ALT/OPT + H' to go to home
+Mousetrap.bindGlobal("ctrl+alt+h", () => {
   if (route.name !== "login") {
     router.push({ name: "home" });
     return false;
